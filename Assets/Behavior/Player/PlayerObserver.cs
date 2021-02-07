a145f4bceca2e1393b68950650f3f42d2d5ab60e
Clockwork-Rat <isaiahraspet@gmail.com>
2021-02-06 17:04:15 -0700

    Some HUD

---
+++ Assets/Behavior/Player/PlayerObserver.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Pirateer;
+
+public class PlayerObserver : MonoBehaviour
+{
+    //player health
+    public float Health { get; set; }
+
+    //health slider
+    public Slider healthSlider;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //later I want to get the player's health from a save file,
+        //but for now it's set to 10 at start
+
+        Health = 10;
+
+        healthSlider.maxValue = 10;
+
+        //make sure user can't slide slider
+        healthSlider.interactable = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(healthSlider.value != Health)
+        {
+            healthSlider.interactable = true;
+            healthSlider.value = Health;
+            healthSlider.interactable = false;
+        }
+
+
+        //test hit
+        if (Input.GetKeyDown(KeyCode.H))
+        {
+            Hit(1.0f);
+        }
+    }
+
+    //hit w/ effects
+    public void Hit(float dmg, List<PEffect> effects)
+    {
+        Health -= dmg;
+    }
+
+    //hit w/o effects
+    public void Hit(float dmg)
+    {
+        Health -= dmg;
+    }
+}
